6332c3ef88a22ac1387bff7280eeb709c006b0da
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-15 21:00:22 -0300

    Add files via upload

---
+++ Uri 1041/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace Uri_1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x=0.0, y=0.0;
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            x = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            y = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+
+            if (x == 0.0 && y == 0.0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x >= 0.0 && y >= 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+
+            else if (x < 0.0 && y >= 0.0) 
+            {
+                Console.WriteLine("Q2");
+            }
+
+            else if (x < 0.0 && y < 0.0) 
+            {
+                Console.WriteLine("Q3");
+            }
+
+            else if (x >= 0.0 && y < 0.0) 
+            {
+                Console.WriteLine("Q4");
+            }
+
+            
+        }
+    }
+}
